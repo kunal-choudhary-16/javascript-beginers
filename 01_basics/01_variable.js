77ed6c8d809016ecbda7cb14feb14660209fc6f7
Kunal <135196470+kunal-choudhary-16@users.noreply.github.com>
2023-09-20 03:19:41 +0000

    container config done

---
+++ 01_basics/01_variable.js
@@ -0,0 +1,6 @@
+const accountId = 13452   //value never change
+let accountmail = "www123@gmail.com"
+var account_name = "sita"    // var is not used in today's programming let can declare block scope variable
+accountcity = "Udaipur";  //it is right but not good way to write
+
+console.table([accountId,accountmail,account_name,accountcity])
