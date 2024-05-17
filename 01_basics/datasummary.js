78df04aa1f650672f081630d555e8ea34998c48d
Kunal <135196470+kunal-choudhary-16@users.noreply.github.com>
2024-05-17 04:57:18 +0000

    stack heap concept

--- 01_basics/datasummary.js
+++ 01_basics/datasummary.js
@@ -33,3 +33,32 @@ const myFunction = function(){
 }
 
 console.log(typeof anotherId);
+
+// Stack - primitive datatype  
+  
+let person1 = "Vikas"
+  
+let person2 = "person1"  //it will show the person1 value but when we change in person2 value and console person1 value it 
+                        //will show original value "vikas" beacause inside stack they made a copy for person2 value.
+
+person2 = "Vishal"
+
+console.log(person1);
+console.log(person2);
+
+// heap - non-primitive/reference datatype
+
+let user1 = {
+    email: "kuku@gooogle.com",
+    Upi : "kukuybl"
+}                           //it will show the user1 value but when we change in user2 value and console person1 value it 
+                           //will show original value that value we changed in user2 beacause inside heap they take value from reference.
+
+let user2 = user1
+
+user2.email = "kunu@google.com"
+
+console.log(user1);
+console.log(user2);
+
+    
