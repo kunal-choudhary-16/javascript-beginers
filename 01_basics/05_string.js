6bd6f8771d32a882ea00ab973e305b5585124679
Kunal <135196470+kunal-choudhary-16@users.noreply.github.com>
2024-05-18 04:23:25 +0000

    string config

---
+++ 01_basics/05_string.js
@@ -0,0 +1,17 @@
+const name = "Kunal"
+const repoCount = 55
+
+// console.log(name + repoCount + " Value"); //but this method is too old
+
+console.log(` Hello my name is ${name} and my repo count is ${repoCount}`); //we can use this backtick form for showing string output
+
+//another method for string through object
+
+const gamename = new String('kunalhc') 
+
+// different methods of string  learn all 
+
+console.log(gamename[0]);  //for accessing the element of object 
+
+console.log(gamename.length);
+console.log(gamename.toUpperCase());
