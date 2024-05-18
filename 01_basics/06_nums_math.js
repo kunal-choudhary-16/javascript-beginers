40fbc0ee1247226b2f7df0c766587797c3d431d4
Kunal <135196470+kunal-choudhary-16@users.noreply.github.com>
2024-05-18 05:01:28 +0000

    nums config

---
+++ 01_basics/06_nums_math.js
@@ -0,0 +1,16 @@
+const score = 400  // one way
+
+const balance = new Number(100)
+console.log(balance);
+
+console.log(balance.toString().length);
+console.log(balance.toFixed(1));  //fixed value after decimal on one 100.0
+
+const otherNumber = 123.78
+
+console.log(otherNumber.toPrecision(3));
+
+
+
+
+ 
