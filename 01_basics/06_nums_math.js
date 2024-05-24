d3578998999b87516e346a71714db9c9ea6f5236
Kunal <135196470+kunal-choudhary-16@users.noreply.github.com>
2024-05-24 04:27:13 +0000

    math config

--- 01_basics/06_nums_math.js
+++ 01_basics/06_nums_math.js
@@ -1,4 +1,4 @@
-const score = 400  // one way
+/*const score = 400  // one way
 
 const balance = new Number(100)
 console.log(balance);
@@ -9,8 +9,14 @@ console.log(balance.toFixed(1));  //fixed value after decimal on one 100.0
 const otherNumber = 123.78
 
 console.log(otherNumber.toPrecision(3));
+console.log(otherNumber.toPrecision(4));     //it is using for round of the value  */
 
+ //console.log(Math.random());
+ //console.log(Math.random() * 10 +1);
 
+ // console.log(Math.floor(Math.random()* 10 + 1));
 
+  const Min = 10;
+  const Max = 20;
 
- 
+  console.log(Math.floor(Math.random() * (Max - Min + 1)) + Min);  //
