7f0495596e8fcce6653002ac6b763a9ae4386e81
Kunal <135196470+kunal-choudhary-16@users.noreply.github.com>
2024-05-28 05:28:56 +0000

    array config

---
+++ 02_basics/01_array.js
@@ -0,0 +1,38 @@
+const myArr = [0,1,2,3,4,5];
+  console.log(myArr);
+// const myheros = ["Shaktiman" , "Avengers" , "Marvel" , "Anime"];
+
+// console.log(myheros[3]);
+ //console.log(myArr[3]);
+
+//method
+
+ // myArr.push(6);     // it will add element in array
+ //console.log(myArr);
+
+ //myArr.pop();      //it will delete element in array , there is no need to pass the argument it will auto delete last inserted value
+ //console.log(myArr);   
+
+/* myArr.unshift(8);    // it will add  given value in starting of array only
+console.log(myArr);
+
+myArr.shift();       // there is no need of argument it will drop the value the in starting of array
+console.log(myArr);  */
+
+//Slice and Splice Two important methods for interview purpose
+
+ const myn1 = myArr.slice(1,3);
+ console.log(myn1);
+
+
+ console.log(myArr);
+ const myn2 = myArr.splice(1,3);
+ console.log(myn2);
+
+
+ console.log(myArr);    //Splice operation  manipulate the array  we see in the output of line 33.  
+
+
+
+
+
